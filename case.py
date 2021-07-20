30195eb647ce179fc128ae2809ea7f9d60fba7d3
Hellobrine <qweqwe.magic@live.ru>
2021-07-20 20:50:23 +0300

    Create case.py

---
+++ case.py
@@ -0,0 +1,14 @@
+from telethon import TelegramClient
+import time
+
+
+api_id = '7971874'
+api_hash = '81d83418ba9b855bcb065ff9620e21f7'
+
+def send():
+	with TelegramClient('anon', api_id, api_hash) as client:
+		client.loop.run_until_complete(client.send_message('https://t.me/joinchat/4Xef62yqUDFmOTAy', 'Кейс открыть 5 1'))
+
+while True:
+	send()
+	time.sleep(5)
