ca763cf35b02aeaa22dbf58b96d98f2265a27aed
Hellobrine <qweqwe.magic@live.ru>
2021-07-22 20:27:17 +0300

    Update auto2.py

--- auto2.py
+++ auto2.py
@@ -4,7 +4,7 @@ import time
 
 api_id = '7971874'
 api_hash = '81d83418ba9b855bcb065ff9620e21f7'
-
+#https://t.me/joinchat/4Xef62yqUDFmOTAy
 def send():
 	with TelegramClient('anon', api_id, api_hash) as client:
 		client.loop.run_until_complete(client.send_message('LesyaTG_bot', 'Кейс открыть 5 3'))
