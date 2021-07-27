adfac9515c2f413f31af2f2dc3d0c37a6715978e
Hellobrine <qweqwe.magic@live.ru>
2021-07-27 08:39:38 +0300

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -8,11 +8,15 @@ api_hash = '81d83418ba9b855bcb065ff9620e21f7'
 def bonus():
 	with TelegramClient('anon', api_id, api_hash) as client:
 		client.loop.run_until_complete(client.send_message('https://t.me/joinchat/4Xef62yqUDFmOTAy', 'Бонус'))
-
+	time.sleep(30)
+	with TelegramClient('anon', api_id, api_hash) as client:
+		client.loop.run_until_complete(client.send_message('https://t.me/joinchat/4Xef62yqUDFmOTAy', 'Бонус'))
 def vipbonus():
 	with TelegramClient('anon', api_id, api_hash) as client:
 		client.loop.run_until_complete(client.send_message('https://t.me/joinchat/4Xef62yqUDFmOTAy', 'Вип бонус'))
-
+	time.sleep(30)
+	with TelegramClient('anon', api_id, api_hash) as client:
+		client.loop.run_until_complete(client.send_message('https://t.me/joinchat/4Xef62yqUDFmOTAy', 'Вип бонус'))
 while True:
 	bonus()
 	time.sleep(60)
