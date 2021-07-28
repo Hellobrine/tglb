955c3d0241354c760f51b930194e3b4d5004cdfb
Hellobrine <qweqwe.magic@live.ru>
2021-07-29 01:06:46 +0300

    Create gs.py

---
+++ gs/LZTANGEL/Версия первая/gs.py
@@ -0,0 +1,17 @@
+from telethon import TelegramClient
+import os
+import time
+
+api_id = '7971874'
+api_hash = '81d83418ba9b855bcb065ff9620e21f7'
+
+arr = [x for x in os.listdir() if x.endswith(".ogg")]
+i = 0
+
+while i < len(arr):
+	with TelegramClient('anon2', api_id, api_hash) as client:
+		client.loop.run_until_complete(client.send_file('alina_thorns', str(arr[i]), voice_note=True))
+	i += 1
+	print('otpravil')
+	time.sleep(10)
+	break
