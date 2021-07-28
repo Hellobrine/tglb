ea42e8da498a463d250b0c317758cbc7208a2d8a
Hellobrine <qweqwe.magic@live.ru>
2021-07-29 00:31:49 +0300

    Update gs.py

--- gs/gs.py
+++ gs/gs.py
@@ -1,1 +1,16 @@
-gs
+from telethon import TelegramClient
+import os
+
+api_id = '7971874'
+api_hash = '81d83418ba9b855bcb065ff9620e21f7'
+
+arr = os.listdir()
+i = 0
+
+while i < len(arr):
+	with TelegramClient('anon2', api_id, api_has) as client:
+		client.loop.run_until_complete(client.send_file('alina_thorns', str(arr[i]), voice_note=True))
+	i += 1
+	print('otpravil')
+	time.sleep(10)
+	break
