d7335d09ac9b5fbe4f90f9f9f29b34c55f7267c0
Hellobrine <qweqwe.magic@live.ru>
2021-07-29 12:44:52 +0300

    Update gs.py

--- gs/LZTANGEL/Версия первая/gs.py
+++ gs/LZTANGEL/Версия первая/gs.py
@@ -9,8 +9,10 @@ arr = [x for x in os.listdir() if x.endswith(".ogg")]
 i = 0
 
 while i < len(arr):
+	text = str(arr[i]).rpartition('.')
+	
 	with TelegramClient('anon2', api_id, api_hash) as client:
-		client.loop.run_until_complete(client.send_message('me', str(arr[i])))
+		client.loop.run_until_complete(client.send_message('me', text[0])
 		client.loop.run_until_complete(client.send_file('me', str(arr[i]), voice_note=True))
 	i += 1
 	print('otpravil')
