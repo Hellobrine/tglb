9409867f9a01af20a443115fbcc18d2172a19294
Hellobrine <qweqwe.magic@live.ru>
2021-07-29 09:20:31 +0300

    Create cnv.py

---
+++ gs/LZTANGEL/Версия первая/cnv.py
@@ -0,0 +1,10 @@
+Import subprocess
+Import os
+
+arr = [x for x in os.listdir() if x.endswith(".mp3")]
+i = 0
+
+while i < len(arr):
+	filename = arr[i]
+	audio_path_ogg = filename + '.ogg'
+	subprocess.run(["ffmpeg", '-i', arr[i], '-acodec', 'libopus', audio_path_ogg, '-y'])
