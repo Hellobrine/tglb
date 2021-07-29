33ce0f2316c4b213624dcae2e52f88a1b0de2b7a
Hellobrine <qweqwe.magic@live.ru>
2021-07-29 09:33:31 +0300

    Update gs.py

--- gs/LZTANGEL/Версия первая/gs.py
+++ gs/LZTANGEL/Версия первая/gs.py
@@ -5,12 +5,12 @@ import time
 api_id = '7971874'
 api_hash = '81d83418ba9b855bcb065ff9620e21f7'
 
-arr = [x for x in os.listdir() if x.endswith(".mp3")]
+arr = [x for x in os.listdir() if x.endswith(".ogg")]
 i = 0
 
 while i < len(arr):
 	with TelegramClient('anon2', api_id, api_hash) as client:
-		client.loop.run_until_complete(client.send_file('mp3toolsbot', str(arr[i])))
+		client.loop.run_until_complete(client.send_file('me', str(arr[i]), voice_note=True))
 	i += 1
 	print('otpravil')
 	time.sleep(10)
