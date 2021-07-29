e7f434621c8ae808da13d112e2ecf2d0d81c00dd
Hellobrine <qweqwe.magic@live.ru>
2021-07-29 12:55:12 +0300

    Update gs.py

--- gs/LZTANGEL/Версия первая/gs.py
+++ gs/LZTANGEL/Версия первая/gs.py
@@ -13,7 +13,7 @@ while i < len(arr):
 	
 	with TelegramClient('anon2', api_id, api_hash) as client:
 		client.loop.run_until_complete(client.send_message('me', text[0])
-		client.loop.run_until_complete(client.send_file('me', str(arr[i]), voice_note=True))
+		client.loop.run_until_complete(client.send_file('me', str(arr[i])), voice_note=True))
 	i += 1
 	print('otpravil')
 	time.sleep(30)
